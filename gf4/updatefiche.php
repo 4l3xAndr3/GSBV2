8658380a6db7736c27d1f89928c885220f229580
l3g0craft <96246326+l3g0craft@users.noreply.github.com>
2023-05-22 09:19:26 +0200

    Add files via upload

---
+++ gf4/updatefiche.php
@@ -0,0 +1,31 @@
+<?php
+include '../fonctiongenevisiteur.php';
+// Connexion    la base de donnes gsb_frais
+$cnxBDD = connexion();
+
+$etape=$_GET['ETP'];
+$km=$_GET['KM'];
+$nuitees=$_GET['NUI'];
+$repas=$_GET['REP'];
+$idfiche=$_GET['idfiche'];
+
+$sql="UPDATE lignefraisforfait SET quantite = $etape WHERE idForfait = 'ETP' AND idFicheFrais=$idfiche;";
+$result=$cnxBDD->query($sql)
+or die ("Requete invalide : ".$sql);
+
+$sql="UPDATE lignefraisforfait SET quantite = $km WHERE idForfait = 'KM' AND idFicheFrais=$idfiche;";
+$result=$cnxBDD->query($sql)
+or die ("Requete invalide : ".$sql);
+
+$sql="UPDATE lignefraisforfait SET quantite = $nuitees WHERE idForfait = 'NUI' AND idFicheFrais=$idfiche;";
+$result=$cnxBDD->query($sql)
+or die ("Requete invalide : ".$sql);
+
+$sql="UPDATE lignefraisforfait SET quantite = $repas WHERE idForfait = 'REP' AND idFicheFrais=$idfiche;";
+$result=$cnxBDD->query($sql)
+or die ("Requete invalide : ".$sql);
+
+
+
+
+?>
